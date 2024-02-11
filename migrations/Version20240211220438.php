b0fd78f538020bc1771ad9f4793b7a3a79ad7344
BahAwa <bah.awaa@gmail.com>
2024-02-11 23:27:46 +0100

    migrations

--- migrations/Version20240211220438.php
+++ migrations/Version20240211220438.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231207133842 extends AbstractMigration
+final class Version20240211220438 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,13 @@ final class Version20231207133842 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE etablissement ADD latitude DOUBLE PRECISION');
-        $this->addSql('ALTER TABLE etablissement ADD longitude DOUBLE PRECISION');
+        $this->addSql('ALTER TABLE etablissement ALTER horaires_ouverture TYPE TEXT');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE etablissement DROP latitude');
-        $this->addSql('ALTER TABLE etablissement DROP longitude');
+        $this->addSql('ALTER TABLE etablissement ALTER horaires_ouverture TYPE VARCHAR(255)');
     }
 }
